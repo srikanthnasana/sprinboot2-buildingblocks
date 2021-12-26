7f09b27497bf7f6f185c01088fb703da3a6e8ce5
srikanth <srikanth.nasana@gmail.com>
2021-12-26 23:57:19 +0530

    add comment

--- src/main/java/com/stacksimply/restservices/repositories/UserRespository.java
+++ src/main/java/com/stacksimply/restservices/repositories/UserRespository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.stacksimply.restservices.entities.User;
 //repository
+//changes
 @Repository
 public interface UserRespository extends JpaRepository<User, Long>{
   User findByUserName(String username);
